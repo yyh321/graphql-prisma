ab56a36e79e5a35d7463d741a97775fb51b1ee0e
yyh123 <yunhaoyu@gmail.com>
2019-07-23 02:55:45 +0800

    搭建prisma开发环境

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const { GraphQLServer } = require('graphql-yoga');
+
+const server = new GraphQLServer({
+  typeDefs: `
+    type Query {
+      hello(name:String):String!
+    }
+  `,
+  resolvers: {
+    Query:{
+      hello:(parent,{name},ctx) => {
+        return `${name},你好，欢迎来一起学习GraphQL!`
+      }
+    }
+  }
+})
+
+server.start({
+  port: 4600
+},({port}) => {
+  console.log(`服务器已启动，请访问:http://localhost:${port}`);
+})
